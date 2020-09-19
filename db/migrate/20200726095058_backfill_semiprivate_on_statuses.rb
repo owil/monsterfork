9d2f75cedc5e89e7089a93ddaaec783cd2539f2e
Fire Demon <firedemon@creature.cafe>
2020-09-19 17:54:06 -0500

    [Database] Fix failing semi-private backfill migration

--- db/migrate/20200726095058_backfill_semiprivate_on_statuses.rb
+++ db/migrate/20200726095058_backfill_semiprivate_on_statuses.rb
@@ -4,7 +4,7 @@ class BackfillSemiprivateOnStatuses < ActiveRecord::Migration[5.2]
   def up
     Rails.logger.info('Backfilling semiprivate statuses...')
     safety_assured do
-      Status.where(id: StatusDomainPermission.select(:status_id).distinct(:status_id)).in_batches.update_all(semiprivate: true)
+      Status.where(id: StatusDomainPermission.select(:status_id)).in_batches.update_all(semiprivate: true)
     end
   end
 
